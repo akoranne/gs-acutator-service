cfb4032c9e23f677e59d0d463264794783628d31
Ajay Koranne <akoranne@gmail.com>
2018-07-31 18:46:19 -0500

    changes to move artifactory variables to secrets

--- ci/tasks/build-service/task.sh
+++ ci/tasks/build-service/task.sh
@@ -7,7 +7,7 @@ export ROOT_FOLDER="$( pwd )"
 export REPO_RESOURCE="service-repo"
 export KEYVAL_RESOURCE=keyval
 export KEYVALOUTPUT_RESOURCE=keyvalout
-export BUILD_OUTPUT="build-output"
+export BUILD_OUTPUT="${ROOT_FOLDER}/../build-output"
 
 echo "Root folder is [${ROOT_FOLDER}]"
 echo "Repo resource folder is [${REPO_RESOURCE}]"
@@ -33,9 +33,9 @@ export GRADLE_OPTS="-Dorg.gradle.native=false"
 # mkdir -f ../build-output
 
 # move all manifests file to target
-cp manifest.yml  ../build-output/
+cp manifest.yml  ${BUILD_OUTPUT}
 
-cp build/libs/*.jar ../build-output/
+cp build/libs/*.jar ${BUILD_OUTPUT}
 
 
 # change dir to the libs folder
@@ -51,17 +51,17 @@ echo " .. current version - ${vers} ";
 cd ../../
 
 # copy the reports of the tests to the output folder
-cp -rfv build/reports  ../build-output/reports
+cp -rfv build/reports  ${BUILD_OUTPUT}/reports
 
 # generate pdf documents from markdown and asciidocs
 echo ""
 echo " .. moving output files to Artifactory"
 markdown-pdf *.md
-mv *.pdf ../build-output
+mv *.pdf ${BUILD_OUTPUT}
 
 echo ""
 echo " .. output files "
-ls -l ../build-output
+ls -l ${BUILD_OUTPUT}
 
 echo " .. TODO - need artifactory endpoints "
 
@@ -75,10 +75,18 @@ group=${group}
 build_number="${build_id}"
 build_name="${atc_external_url}/teams/${build_team_name}/pipelines/${build_pipeline_name}"
 
-### TODO move to secrets
-jfrog rt config rt-server-1 --user=admin --password=password --url=http://artifactory:8081/artifactory
+# cd to the output folder
+cd ${BUILD_OUTPUT}
+
+jfrog rt config "${JFROG_SERVER}" \
+    --user="${JFROG_USER}" --password="${JFROG_PASSWORD}" \
+    --url="${JFROG_URL}"
+
+# show
 jfrog rt show
-jfrog rt u "../build-output/*" "example-repo-local/${group}/${vers}/" \
+
+# copy all files from current location
+jfrog rt u "*" "${JFROG_LOCATION}/${group}/${vers}/" \
   --flat=false \
   --build-name="${build_name}" \
   --build-number="${build_number}"
