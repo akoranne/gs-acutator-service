3719c039a5e37b18ed16c68341ff9b5d365b25f3
Ajay Koranne <akoranne@gmail.com>
2018-07-31 16:52:39 -0500

    testing build-upload

--- ci/tasks/build-service/task.sh
+++ ci/tasks/build-service/task.sh
@@ -12,30 +12,29 @@ propsFile="${propsDir}/keyval.properties"
 mkdir -p "${ROOT_FOLDER}/${KEYVALOUTPUT_RESOURCE}"
 touch "${propsFile}"
 
-
 echo ""
 echo " .. Running build"
 echo ""
-export atc_external_url="$(cat metadata/atc-external-url)";
-export build_id="$(cat metadata/build-id)";
-export build_job_name="$(cat metadata/build-job-name)";
-export build_name="$(cat metadata/build-name)";
-export build_pipeline_name="$(cat metadata/build-pipeline-name)";
-export build_team_name="$(cat metadata/build-team-name)";
-
-echo "atc_external_url=${atc_external_url}" >> "${propsFile}"
-echo "build_id=${build_id}" >> "${propsFile}"
-echo "build_job_name=${build_job_name}" >> "${propsFile}"
-echo "build_name=${build_name}" >> "${propsFile}"
-echo "build_pipeline_name=${build_pipeline_name}" >> "${propsFile}"
-echo "build_team_name=${build_team_name}" >> "${propsFile}"
-
-echo "url - ${atc_external_url}"
-echo "build-id - ${build_id}"
-echo "job - ${build_job_name}"
-echo "build - ${build_name}"
-echo "pipeline - ${build_pipeline_name}"
-echo "team - ${build_team_name}"
+# export atc_external_url="$(cat metadata/atc-external-url)";
+# export build_id="$(cat metadata/build-id)";
+# export build_job_name="$(cat metadata/build-job-name)";
+# export build_name="$(cat metadata/build-name)";
+# export build_pipeline_name="$(cat metadata/build-pipeline-name)";
+# export build_team_name="$(cat metadata/build-team-name)";
+#
+# echo "atc_external_url=${atc_external_url}" >> "${propsFile}"
+# echo "build_id=${build_id}" >> "${propsFile}"
+# echo "build_job_name=${build_job_name}" >> "${propsFile}"
+# echo "build_name=${build_name}" >> "${propsFile}"
+# echo "build_pipeline_name=${build_pipeline_name}" >> "${propsFile}"
+# echo "build_team_name=${build_team_name}" >> "${propsFile}"
+#
+# echo "url - ${atc_external_url}"
+# echo "build-id - ${build_id}"
+# echo "job - ${build_job_name}"
+# echo "build - ${build_name}"
+# echo "pipeline - ${build_pipeline_name}"
+# echo "team - ${build_team_name}"
 
 exit 1;
 
