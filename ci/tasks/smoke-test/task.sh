edb3cea578287b039b56cc4c75bb349313fdc44f
Ajay Koranne <akoranne@gmail.com>
2018-08-01 17:39:48 -0500

    added smoke-tests

---
+++ ci/tasks/smoke-test/task.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -e # fail fast
+# set -x # print commands
+
+export ROOT_FOLDER="$( pwd )"
+export REPO_RESOURCE="service-repo"
+export KEYVAL_RESOURCE=keyval
+export KEYVALOUTPUT_RESOURCE=keyvalout
+export BUILD_OUTPUT="${ROOT_FOLDER}/build-output"
+
+propsDir="${ROOT_FOLDER}/${KEYVALOUTPUT_RESOURCE}"
+propsFile="${propsDir}/keyval.properties"
+
+echo "Root folder is [${ROOT_FOLDER}]"
+echo "Build output folder is [${BUILD_OUTPUT}]"
+echo "Repo resource folder is [${REPO_RESOURCE}]"
+echo "KeyVal resource folder is [${KEYVAL_RESOURCE}]"
+
+# import common functions
+source "${ROOT_FOLDER}/${REPO_RESOURCE}/ci/tasks/resource-utils.sh"
+
+# export passed properties to env
+exportKeyValProperties
+
+echo ""
+echo " .. Running smoke-test for ${TARGET_ENV}"
+echo ""
+
+cd $REPO_RESOURCE
+
+# execute newman tests
+newman run src/main/resources/postman/smoke-test.collection.dev | tee ../test-output/smoke_test_dev_results.out
+
+echo ""
+echo " .. output files "
+ls -l ../test-output
+
+echo ""
+echo " Smoke Test completed!!!"
+echo ""
+
+# write passed properties out
+passKeyValProperties
